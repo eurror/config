aeb654c3aea5f2d8465924ece23372fa6bbb795c
eurus <evr@gmx.us>
2025-08-11 15:05:18 +0500

    changed toggle-term confgis

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -257,7 +257,12 @@ require("lazy").setup({
     {
         "akinsho/toggleterm.nvim",
         version = "*",
-        config = true
+        config = function()
+            require("toggleterm").setup({
+                size=15,
+                open_mapping = [[<C-`>]],
+            })
+        end,
     },
 
     {
