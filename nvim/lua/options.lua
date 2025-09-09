b1d1238fca68587aefbbf0cd202187221e080e1b
eurus <68112241+eurror@users.noreply.github.com>
2025-09-09 15:48:47 +0500

    Neovim: refactored "options.lua"

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,16 +1,33 @@
--- lua/core/options.lua
 local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+
 opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
-opt.guicursor = ""
-opt.termguicolors = true
 opt.smartindent = true
+
+opt.termguicolors = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.wrap = true
+opt.guicursor = ""
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = true
+
 opt.mouse = "a"
 opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.updatetime = 250
+opt.timeoutlen = 400
 
--- Suppress deprecation warnings (temporary solution)
-vim.deprecate = function() end
+opt.splitright = true
+opt.splitbelow = true
