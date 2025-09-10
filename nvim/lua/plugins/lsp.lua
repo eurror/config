b593a014b29fed989a439628b30623268e6903c8
eurus <68112241+eurror@users.noreply.github.com>
2025-09-10 15:46:42 +0500

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -30,7 +30,14 @@ return {
         },
         config = function()
             local servers = {
-                pyright = {},
+                pyright = {
+                    settings = {
+                        python = {
+                            venvPath = ".",
+                            pythonPath = './venv/bin/python'
+                        }
+                    }
+                },
                 jsonls = {},
                 html = {},
                 cssls = {},
@@ -51,6 +58,7 @@ return {
 
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            capabilities.textDocument.positionEncoding = { "utf-8" }
 
             require("mason").setup({
                 ui = {
@@ -68,11 +76,19 @@ return {
                 automatic_installation = true,
             })
 
-            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-            end
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN]  = " ",
+                        [vim.diagnostic.severity.HINT]  = " ",
+                        [vim.diagnostic.severity.INFO]  = " ",
+                    },
+                },
+                virtual_text = true,
+                underline = true,
+                update_in_insert = false,
+            })
 
             local on_attach = function(_, bufnr)
                 local opts = { buffer = bufnr, silent = true }
@@ -90,5 +106,23 @@ return {
                 }, config))
             end
         end,
-    }
+    },
+
+    {
+        "nvimtools/none-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                    null_ls.builtins.diagnostics.ruff,
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.prettier,
+                },
+            })
+        end,
+    },
 }
