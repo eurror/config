63d29fc72d68712062fdb1d8aa04f8fea3a44c9a
eurus <68112241+eurror@users.noreply.github.com>
2025-09-10 11:24:56 +0500

    Neovim: this one works (fixes after separating into different files)

--- nvim/lua/plugins/notifications.lua
+++ nvim/lua/plugins/notifications.lua
@@ -1,39 +1,41 @@
-{
-    "rcarriga/nvim-notify",
-    event = "VeryLazy",
-    config = function()
-        require("notify").setup({ background_colour = "#000000" })
-        vim.notify = require("notify")
-    end,
-},
-{
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-    config = function()
-        require("noice").setup({
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
+return {
+    {
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        config = function()
+            require("notify").setup({ background_colour = "#000000" })
+            vim.notify = require("notify")
+        end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
                 },
-            },
-            presets = {
-                command_palette = true,
-                long_message_to_split = true,
-                lsp_doc_border = true,
-            }
-        })
-    end,
-},
-{
-    "folke/flash.nvim",
-    config = function() require("flash").setup() end,
-},
-{
-    "folke/trouble.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function() require("trouble").setup() end,
-},
+                presets = {
+                    command_palette = true,
+                    long_message_to_split = true,
+                    lsp_doc_border = true,
+                }
+            })
+        end,
+    },
+    {
+        "folke/flash.nvim",
+        config = function() require("flash").setup() end,
+    },
+    {
+        "folke/trouble.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function() require("trouble").setup() end,
+    },
+}
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,36 +1,38 @@
-{
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme vscode")
-    end,
-},
-{
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup()
-    end,
-},
-{
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        require("bufferline").setup({
-            options = {
-                diagnostics = "nvim_lsp",
-                show_duplicate_prefix = true,
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        text_align = "left",
-                        separator = true,
-                        highlight = "Directory",
-                    }
-                },
-            }
-        })
-    end,
-},
+return {
+    {
+        "Mofiqul/vscode.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme vscode")
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup()
+        end,
+    },
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("bufferline").setup({
+                options = {
+                    diagnostics = "nvim_lsp",
+                    show_duplicate_prefix = true,
+                    offsets = {
+                        {
+                            filetype = "NvimTree",
+                            text = "File Explorer",
+                            text_align = "left",
+                            separator = true,
+                            highlight = "Directory",
+                        }
+                    },
+                }
+            })
+        end,
+    },
+}
