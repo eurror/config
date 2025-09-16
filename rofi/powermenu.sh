7861edb8b11945d066cbee052b4a4699430061d5
eurror <evr@gmx.us>
2025-09-16 14:45:42 +0500

    General: - improved `.gitignore` - added powermenu for rofi

---
+++ rofi/powermenu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+chosen=$(echo -e "󰍃 Lock\n󰗼 Logout\n󰜉 Reboot\n⏻ Shutdown" \
+  | rofi -dmenu -i -p "System Action:" \
+    -theme-str 'window {location: center;}')
+
+case "$chosen" in
+  "󰍃 Lock") i3lock -c 04090e ;;
+  "󰗼 Logout") i3-msg exit ;;
+  "󰜉 Reboot") systemctl reboot ;;
+  "⏻ Shutdown") systemctl poweroff ;;
+esac
