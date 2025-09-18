7b4c78dbd1c5f245ec3534247cd72a45cfdc168e
eurror <evr@gmx.us>
2025-09-18 17:19:32 +0500

    i3: lock screen

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# convert ~/.config/i3/wallpaper.png -resize 10% -blur 0x6 -resize 500% ~/.config/i3/lockscreen.png
+
 i3lock \
-  -i ~/.config/i3/wallpaper.png \
+  -i ~/.config/i3/lockscreen.png \
   --nofork \
   --tiling \
   --ignore-empty-password \
