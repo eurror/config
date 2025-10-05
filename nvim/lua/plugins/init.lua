0fcfdf820e98902225a2a6181e36cf7cd1e4af1e
eurus <evr@gmx.us>
2025-10-05 17:23:27 +0500

    Neovim: disabled all except neotree; helix: saving auto focus lose

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,37 +1,39 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Собираем все модули из папки plugins
 local plugins = {}
 local plugin_files = {
-    "plugins.ui",
-    "plugins.lsp",
-    "plugins.git",
-    "plugins.cmp",
-    "plugins.files",
-    "plugins.debug",
-    "plugins.notifications",
-    "plugins.ux",
-    "plugins.disabled",
+	"plugins.neotree",
+	"plugins.telescope",
+	-- "plugins.ui",
+	-- "plugins.lsp",
+	-- "plugins.git",
+	-- "plugins.cmp",
+	-- "plugins.files",
+	-- "plugins.debug",
+	-- "plugins.notifications",
+	-- "plugins.ux",
+	-- "plugins.disabled",
 }
 
 for _, file in ipairs(plugin_files) do
-    local ok, mod = pcall(require, file)
-    if ok and type(mod) == "table" then
-        for _, plugin in ipairs(mod) do
-            table.insert(plugins, plugin)
-        end
-    end
+	local ok, mod = pcall(require, file)
+	if ok and type(mod) == "table" then
+		for _, plugin in ipairs(mod) do
+			table.insert(plugins, plugin)
+		end
+	end
 end
 
 require("lazy").setup(plugins)
