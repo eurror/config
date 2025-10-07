bd9e9bba7d350e1de448907388b50738a16848a8
eurus <evr@gmx.us>
2025-10-07 15:50:48 +0500

    Neovim: git

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,80 +1,42 @@
 return {
-    { "tpope/vim-fugitive", cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit", "Gblame", "GBrowse" } },
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "BufReadPre",
-        config = function()
-            require("gitsigns").setup({
-                signs = {
-                    add = { text = "│" },
-                    change = { text = "│" },
-                    delete = { text = "_" },
-                    topdelete = { text = "‾" },
-                    changedelete = { text = "~" },
-                },
-                current_line_blame = true,
-                current_line_blame_opts = {
-                    delay = 300,
-                    virt_text_pos = "eol",
-                },
-                on_attach = function(bufnr)
-                    local gs = package.loaded.gitsigns
-                    local map = function(mode, lhs, rhs, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, lhs, rhs, opts)
-                    end
-                    map("n", "]c", function()
-                        if vim.wo.diff then return "]c" end
-                        vim.schedule(gs.next_hunk)
-                        return "<Ignore>"
-                    end, { expr = true })
-
-                    map("n", "[c", function()
-                        if vim.wo.diff then return "[c" end
-                        vim.schedule(gs.prev_hunk)
-                        return "<Ignore>"
-                    end, { expr = true })
-
-                    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-                    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-                    map("n", "<leader>hS", gs.stage_buffer)
-                    map("n", "<leader>hu", gs.undo_stage_hunk)
-                    map("n", "<leader>hR", gs.reset_buffer)
-                    map("n", "<leader>hp", gs.preview_hunk)
-                    map("n", "<leader>hb", function() gs.blame_line { full = true } end)
-                end,
-            })
-        end,
+  { "tpope/vim-fugitive", cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit", "Gblame", "GBrowse" } },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+      })
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("diffview").setup()
+      vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "Diffview Open" })
+      vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "Diffview Close" })
+      vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview Focus Files" })
+      vim.keymap.set("n", "<leader>dt", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview Toggle Files" })
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<CR>", desc = "Open LazyGit" },
     },
-    {
-        "sindrets/diffview.nvim",
-        cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("diffview").setup()
-            vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "Diffview Open" })
-            vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "Diffview Close" })
-            vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview Focus Files" })
-            vim.keymap.set("n", "<leader>dt", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview Toggle Files" })
-        end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
     },
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = "LazyGit",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        keys = {
-            { "<leader>gg", "<cmd>LazyGit<CR>", desc = "Open LazyGit" },
-        },
-    },
-    {
-      "NeogitOrg/neogit",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim",
-        "nvim-telescope/telescope.nvim",
-        "ibhagwan/fzf-lua",
-        "echasnovski/mini.pick",
-      },
-    }
+  }
 }
