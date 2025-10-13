e297c73184814aa95d1a9a25ff75bd906ec772f9
eurror <evr@gmx.us>
2025-10-13 18:43:58 +0500

    Neovim: shift+r like in helix

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Replace selection with last yanked text, preserving the yank buffer
+vim.keymap.set("x", "R", '"_dP', { desc = "Replace selection with yank (Helix-style)" })
