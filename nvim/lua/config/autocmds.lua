9ef57134f1d0ea95381e00e7c80467edd0c89edc
eurror <evr@gmx.us>
2025-10-15 18:20:36 +0500

    Nvim: dap-ui modifiable

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "dap-repl",
+    "dapui_console",
+    "dapui_watches",
+    "dapui_stacks",
+    "dapui_breakpoints",
+    "dapui_scopes",
+    "dapui_hover",
+  },
+  callback = function(event)
+    vim.bo[event.buf].modifiable = true
+  end,
+})
