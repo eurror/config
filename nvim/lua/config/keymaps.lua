d810ed12ab44ff961bb7ae9d4e6011b3c47ab4c8
eurror <evr@gmx.us>
2025-10-15 13:46:08 +0500

    Kitty: updated theme, nvim: keybinding and disabled auto-formatting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,5 @@
 
 -- Replace selection with last yanked text, preserving the yank buffer
 vim.keymap.set("x", "R", '"_dP', { desc = "Replace selection with yank (Helix-style)" })
+vim.keymap.set({"n","t"}, "<c-`>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set({"n", "i", "v"}, "<A-Return>", "<Esc>o")
